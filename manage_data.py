2d7868678282e849dbc117d2e5f1d7403bb16f22
Behrad Koohy <43882475+behradkoohy@users.noreply.github.com>
2020-04-08 14:20:08 +0100

    added some more stuff

--- manage_data.py
+++ manage_data.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.svm import SVC
 
 def get_datasets():
     brca = pd.read_csv('data/Gistic2_CopyNumber_BRCA', sep='\t')
@@ -14,3 +16,15 @@ def get_datasets():
     print(len(df), len(brca.transpose()-2) + len(ovca.transpose()-2))
     return df
 
+# def plot_coefficients_linSVC(classifier, feature_names, top_features=20):
+#     coef = classifier.coef_.ravel()
+#     top_positive_coefficients = np.argsort(coef)[-top_features:]
+#     top_negative_coefficients = np.argsort(coef)[:top_features]
+#     top_coefficients = np.hstack([top_negative_coefficients, top_positive_coefficients])
+#     # create plot
+#     plt.figure(figsize=(15, 5))
+#     colors = ['red' if c < 0 else 'blue' for c in coef[top_coefficients]]
+#     plt.bar(np.arange(2 * top_features), coef[top_coefficients], color=colors)
+#     feature_names = np.array(feature_names)
+#     plt.xticks(np.arange(1, 1 + 2 * top_features), feature_names[top_coefficients], rotation=60, ha='right')
+#     plt.show()
